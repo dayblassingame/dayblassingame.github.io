376532f4874ab4e4c53213dccea0b43cccb581f2
Day Blassingame <dayblassingame@gmail.com>
2024-01-26 23:20:44 -0500

    nav added styled for mobile

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,20 +5,15 @@ import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 export function Header(){
     return(
-        <header>
-            <span>
-                <img src={Logo} alt="Logo"/>
+        <header className="p-C-header">
+            <span className="p-C-logo_span ">
+                <a href="#"><img src={Logo} alt="Logo" className="p-C-logo_img"/></a>
             </span>
-            <span>
-                <FontAwesomeIcon icon={faBars} />            
-            </span>
-            <span>
-                <a href=''>Home</a>
-                <a href=''>About Me</a>
-                <a href=''>Projects</a>
-                <a href=''>Contact</a>
-            </span>
+            <div className="p-C-navBar">
+                <span><a href=''>About</a></span>
+                <span><a href=''>Projects</a></span>
+                <span><a href=''>Contact</a></span>
+            </div>
         </header>
-
     )
 }
